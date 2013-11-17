d81c4ded5e9d8b39d22d6b7baa2575bc656c1c37
dubaokun <dubaokun@esrichina.com.cn>
2013-11-17 23:30:45 +0800

    Refactor all the code \Add readme

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-PiesLayer
+可交互的饼状图 PiesLayer  API
 =========
 
-ArcGIS API  for JavaScript 
+PiesLayer API  基于ArcGIS API for Javascript 实现。作为一个扩展模块，用于在地图上加载可交互的饼状图，用另一种方式实现数据的可视化展示。
+
+介绍：
+PiesLayer 是扩展的GraphicsLayer，因此GraphicsLayer中原有的方法均可实现
+
+构造函数: PiesLayer(option) ，包含一个JSON对象类型的参数。具体内容如下。
+	 r  数字类型  可选 如果忽略该参数则使用默认值为100
+    colors  数组类型，必选 ，这个也是唯一的一个必选参数。格式如{'name1':"#F7464A",'name2':'#E2EAE9','name3':'#02EAF9','name4':'#D4CCC5','name5':'#D4CC00'}
+	linecolor 字符串类型 可选。饼图边框的颜色，可以为16进制形式，如#FFFFFF，也可以使用颜色的名字，如red，还可以通过rgba构造如'rgba(200, 200, 200, 0.75)'，默认是值为'rgba(229, 205, 205, 0.78)'
+    selectedlinecolor 字符串类型 可选。饼图被选中部分的边框颜色。默认值为 "#0F66E9".
+    data 对象数组 可选 。作为饼图的数据的数组。格式如[{x: 13567611.673366752,y:5174359.8470072355,attributes:{'name':'AA地区','指标一':30,'指标二':40,'指标三':50,'指标四':60,'指标五':70}},{x: 13567611.673366752,y:5174359.8470072355,attributes:{'name':'AA地区','指标一':30,'指标二':40,'指标三':50,'指标四':60,'指标五':70}}]
+    spatialReference 空间参考，默认为web mecartor。
+
+主要的方法(仅列出与GraphicsLayer差别的方法)。
+	add(p) 增加pie到Layer中，p的格式为{x: 13567611.673366752,y:5174359.8470072355,attributes:{'name':'AA地区','指标一':30,'指标二':40,'指标三':50,'指标四':60,'指标五':70}}
+	setPieRadius(r) 改变饼图的半径。
+
+事件
+与GraphicsLayer的事件一致。主要对lick ，mouse-move ，mouse-over，mouse-down mouse-up等事件的参数进行了扩展。可以获得 被选择饼图的数据源，以及被选择部分的数据。通过监听这些事件获得被选择部分的数据以达到交互的目的。如可以进行弹出infowindow 等其他操作。
+
+示例
+
+首先在dojoConfig里面配置 扩展包的路径参数
+	 var dojoConfig = { 
+        parseOnLoad: true,
+        packages: [{
+          "name": "extras",
+          "location": location.pathname.replace(/\/[^/]+$/, '') + "/extras"
+        }]
+      };
+
+接下来的代码也很简单。类似在graphicLayer中添加graphic。
+
+	  require(["esri/map", "esri/layers/ArcGISTiledMapServiceLayer", "dojo/dom", "dojo/on", "dojo/dom-class", "extras/PiesLayer","dojo/domReady!"],
+	  function(Map, ArcGISTiledMapServiceLayer, dom, on, domClass,PiesLayer) {
+	    var map = new Map("map", {
+	      basemap: "topo",
+	      center: [100.69828872684525, 34.24237112174851], // long, lat
+	      zoom: 5,
+	      sliderStyle: "small"
+	    });
+	 var data1={x: 13567611.673366752,y:5174359.8470072355,attributes:{'name':'AA地区','指标一':30,'指标二':40,'指标三':50,'指标四':60,'指标五':70}};
+	 var data2={x: 12041317.092568759,y:4988464.994217737,attributes:{'name':'BB地区','指标一':50,'指标二':60,'指标三':20,'指标四':160,'指标五':80}};
+	 var data3={x: 11131410.707862262,y:4328049.069833989,attributes:{'name':'CC地区','指标一':50,'指标二':60,'指标三':120,'指标四':60,'指标五':80}};
+ 	var data4={x: 13303445.303613253,y:3594253.5982964924,attributes:{'name':'DD地区','指标一':50,'指标二':60,'指标三':20,'指标四':60,'指标五':80}};
+ 	var data5={x: 11782042.69262551,y:3061028.8889792445,attributes:{'name':'EE地区','指标一':250,'指标二':60,'指标三':120,'指标四':60,'指标五':80}};
+ 	var data6={x: 10485670.692909265,y:4010071.03216774,attributes:{'name':'FF地区','指标一':150,'指标二':60,'指标三':20,'指标四':60,'指标五':80}};
+ 	var data7={x: 8749021.410270521,y:5052060.601750986,attributes:{'name':'GG地区','指标一':50,'指标二':160,'指标三':20,'指标四':60,'指标五':80}};
+	 var colors={'指标一':"#F7464A",'指标二':'#E2EAE9','指标三':'#02EAF9','指标四':'#D4CCC5','指标五':'#D4CC00'};
+	//构造 PiesLayer
+ 	var pielayer= new PiesLayer({colors:colors,r:120,data:[data1,data2,data3,data4,data5]});
+        map.addLayer(pielayer);
+       //or 有也可以通过 pielayer.add()来动态地添加Pie 比如
+       pielayer.add(data6);
+       pielayer.add(data7);
+       //在click ，mouse-move ，mouse-over，mouse-down mouse-up事件里 可以获得被点击哪一个Pie的 哪一部分 如下
+       on(pielayer,'click',function(e){
+        var piedata=e.piedata; //通过e.piedata可以获得当前区域的所有的数据的集合如{'name':'AA地区','指标一':30,'指标二':40,'指标三':50,'指标四':60,'指标五':70}
+        var  name=piedata.name;
+        var slecetedata=e.slecetedata;    
+        map.infoWindow.setContent(name+"   "+JSON.stringify(slecetedata));
+         map.infoWindow.show(e.mapPoint);
+       }); 
+	  });
---
+++ extras/PiesLayer.js
@@ -0,0 +1,255 @@
+define(["dojo/_base/declare", "esri/layers/GraphicsLayer", "esri/geometry/Point", "esri/SpatialReference",
+  "esri/graphic", "esri/symbols/PictureMarkerSymbol","esri/geometry/webMercatorUtils","dojo/_base/lang"
+], function(declare, GraphicsLayer, Point, SpatialReference,Graphic, PictureMarkerSymbol,webMercatorUtils,lang) {
+  
+
+  var Pie = function Pie(data, colorset, height,linecolor,selectedlinecolor) {
+    this.angles = [];
+    this.base64data = "";
+    //  data:{'name1':30,'name2':40,'name3':50,'name4':60,'name5':70}
+    this.data = data;
+    this.linecolor=linecolor||"rgba(229, 205, 205, 0.78)";   
+    this.selectedlinecolor=selectedlinecolor||"#0F66E9"; // format suchas  #FFFFFF rgba(229, 205, 205, 0.78) or color name
+    this.config = colorset; //{'name1':"#F7464A",'name2':'#E2EAE9','name3':'#02EAF9','name4':'#D4CCC5','name5':'#D4CC00'}
+    this.canvas = document.createElement('canvas');
+    this.height= height;
+    this.canvas.height= height;
+    this.canvas.width = height;
+    this.ctx = this.canvas.getContext('2d');
+    this.pieRadius = height / 2 - 5;
+    this.segmentTotal = 0;
+    for (var i in data) {
+      if(this.config.hasOwnProperty(i))
+      this.segmentTotal += this.data[i];
+    }
+    console.log("segmentTotal"+this.segmentTotal);
+    var startangle = 0
+    for (var d in data) {
+   if(this.config.hasOwnProperty(d)){
+       this.angles.push({
+        name: d,
+        angle: startangle + (this.data[d] / this.segmentTotal) * (Math.PI * 2)
+      });
+      startangle = startangle + (this.data[d] / this.segmentTotal) * (Math.PI * 2);
+   }
+}
+    this.draw(-1);
+  }
+   Pie.prototype.setPieRadius=function(r){
+      this.height= r;
+      this.canvas.height= r;
+      this.canvas.width = r;
+      this.pieRadius = r/2 - 5;
+   }
+  Pie.prototype.getAreaIndex = function(y, x) {
+    var angle = Math.atan2(y, x);
+    if (angle < 0) angle = -angle;
+    else {
+      angle = Math.PI * 2 - angle;
+    }
+    if (angle > this.angles[this.angles.length - 1]) {
+      return this.angles.length - 1;
+    }
+    for (var i = 0; i < this.angles.length; i++) {
+      if (angle < this.angles[i].angle) {
+        return i;
+      }
+    }
+  }
+  Pie.prototype.getImageData = function() {
+    return this.canvas.toDataURL("image/png");
+  }
+  Pie.prototype.updateImageData = function(data) {
+
+  }
+  Pie.prototype.draw = function() {
+    var ctx = this.ctx;
+    ctx.clearRect(0, 0, this.height, this.height);
+    var startAngle = 0;
+    for (var i = 0; i < this.angles.length; i++) {
+      {
+        ctx.strokeStyle =this.linecolor; 
+        ctx.beginPath();
+        ctx.arc(this.height / 2, this.height / 2, this.pieRadius, startAngle, this.angles[i].angle, false);                                
+        ctx.lineTo(this.height / 2, this.height / 2);
+        ctx.closePath();
+        ctx.fillStyle = this.config[this.angles[i].name];
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        startAngle = this.angles[i].angle;
+      }
+    }
+  }
+  Pie.prototype.drawselected = function(i) {
+    var ctx = this.ctx;    
+    this.draw();
+    ctx.beginPath()
+    ctx.strokeStyle = this.selectedlinecolor;
+    ctx.lineWidth = 2;
+    if (i == 0) {
+      ctx.arc(this.height / 2, this.height / 2, this.pieRadius, 0, this.angles[0].angle, false); 
+    } else {
+      ctx.arc(this.height / 2, this.height / 2, this.pieRadius, this.angles[i - 1].angle, this.angles[i].angle, false); //逆时针
+    }
+    ctx.lineTo(this.height / 2, this.height / 2);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.lineWidth = 2;
+    ctx.strokeStyle =this.linecolor; 
+  }
+
+
+  return declare([GraphicsLayer], {
+   
+    constructor: function(options) {
+     // r  Number?  
+     //   Optional .the radius of the pie
+     //colors  Object 
+     //Required:the color set of the pie such as {'name1':"#F7464A",'name2':'#E2EAE9','name3':'#02EAF9','name4':'#D4CCC5','name5':'#D4CC00'}
+     //linecolor String?
+     //   Optional.: the borderline color of  the pie. Hex string or array of rgba values used as the color for cluster labels. Default value is 'rgba(229, 205, 205, 0.78)'.
+     //selectedlinecolor String?
+     //   Optional. the borderline color of  when the pie is selected .Hex string or array of rgba values used as the color for cluster labels. Default value is '"#0F66E9"'.
+     //   data Object[]
+     //    Array of objects. Required. Object are required to have properties named x, y and attributes. The x and y coordinates have to be numbers that represent a points coordinates
+     //   spatialReference the spatialReference of the pielayer;  default value is Web Mercator coordinates
+      this.colorConfig = options.colors;
+      this.pieRadius = options.r;
+      this.linecolor=options.linecolor||"rgba(229, 205, 205, 0.78)";   
+      this.selectedlinecolor=options.selectedlinecolor||"#0F66E9"; 
+      this.spatialReference=options.spatialReference|| new SpatialReference({wkid:102100});
+      this.data=options.data||[];
+    },
+   _setMap: function(map, surface){
+    
+       if(this.data){
+        this.data.forEach(lang.hitch(this,function(d){
+                this.add(d);
+        }));       
+       }
+       var div = this.inherited(arguments);
+    return div;
+    },
+    //{x: 11547228.141733509,y:4641746.633916269,spatialReference:new SpatialReference({wkid:102100}),data:{'name1':30,'name2':40,'name3':50,'name4':60,'name5':70}};
+    addpie: function(e) {
+      //this.data.push(e);
+      if (e.x && e.y && e.attributes) {
+          var x,y,pt;
+
+ if(e.x>-180&&e.x<180&&e.y>-90&&e.y<90){
+         var normalizedVal = webMercatorUtils.lngLatToXY(e.x, e.y, true);
+         x=normalizedVal[0];
+         y=normalizedVal[1]
+        }
+        else
+        {
+          x=e.x;y=e.y;
+        }
+   
+        pt = new Point(e.x,e.y, this.spatialReference);         
+        var pie = new Pie(e.attributes, this.colorConfig, this.pieRadius + 5);
+        var imagedata = pie.getImageData();
+        var json = {
+          url: imagedata,
+          "width": pie.height,
+          "height": pie.height
+        };
+        var sym = new PictureMarkerSymbol(json)
+        var graphic = new Graphic(pt, sym,e.attributes);
+        graphic.haschart = true;
+        graphic.pie = pie;
+        graphic.selectedindex = -1;
+        //  this.inherited(graphic);
+        this.add(graphic);
+      }
+    },
+    add: function(p) {
+      if (p.haschart) {
+        this.inherited(arguments);
+        this.onGraphicAdd(arguments);
+        return;
+      }
+      this.addpie(p);
+    },
+    onGraphicAdd: function() {},
+     _updatesymbol:function(e,unselected) {
+      if (e.graphic.haschart) {
+        var graphic = e.graphic;
+        var mappt = e.mapPoint;
+        var y = mappt.y - graphic.geometry.y;
+        var x = mappt.x - graphic.geometry.x;
+        var pie = e.graphic.pie;
+        if (e.graphic.selectedindex != pie.getAreaIndex(y, x)) {
+            e.graphic.selectedindex = pie.getAreaIndex(y, x);
+            var sname=pie.angles[e.graphic.selectedindex].name;          
+              e.slecetedata={};
+              e.slecetedata[sname]=pie.data[sname];
+              e.piedata=pie.data;               
+              pie.drawselected(pie.getAreaIndex(y, x));      
+          var imagedata = pie.getImageData();
+          var json = {
+            url: imagedata,
+            "width": pie.height,
+            "height": pie.height
+          };
+          var sym = new PictureMarkerSymbol(json)
+          graphic.setSymbol(sym)
+        }
+      }
+    },
+   onClick: function(e) {
+     this._updatesymbol(e);
+      this._extenteventarg(e);
+    },
+    onMouseMove: function(e) {
+       this._updatesymbol(e);
+       this._extenteventarg(e);
+    },
+    onMouseOver: function(e) {
+      this._updatesymbol(e);
+      this._extenteventarg(e);
+    },
+    onMouseDown: function(e) {
+        this._extenteventarg(e);
+    },
+    onMouseUp: function() {
+         this._extenteventarg(e);
+    },
+    _extenteventarg:function(e){
+     var pie = e.graphic.pie;
+     if(e.graphic.selectedindex>=0){
+       var sname=pie.angles[e.graphic.selectedindex].name;  
+       e.slecetedata={};
+       e.slecetedata[sname]=pie.data[sname];
+       e.piedata=pie.data;  
+     }      
+    },
+    setPieRadius:function(r){
+            this.graphics.forEach(function(graphic,i){
+              if(graphic.haschart)graphic.pie.setPieRadius(r)
+      });
+      this.graphics.forEach(function(graphic,i){
+        if(graphic.haschart)graphic.pie.draw();
+      });
+    },
+    onMouseOut: function(e) {
+    if (e.graphic.haschart) {
+        var graphic = e.graphic;       
+        var pie = e.graphic.pie;  
+              pie.draw();     
+          var imagedata = pie.getImageData();
+          var json = {
+            url: imagedata,
+            "width": pie.height,
+            "height": pie.height
+          };
+          var sym = new PictureMarkerSymbol(json)
+          graphic.setSymbol(sym)
+          e.graphic.selectedindex=-1;
+      }
+      this._extenteventarg(e);
+    }
+  
+  })
+});
